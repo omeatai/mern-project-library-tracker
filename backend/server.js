d7a5b66d79d926cf26bffd72e2f62a55d78888a7
omeatai <omeatai@gmail.com>
2023-11-13 07:33:41 -0700

    4. Connect to MongoDB Database with Mongoose
    4. Connect to MongoDB Database with Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-// const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -10,7 +10,14 @@ const port = process.env.PORT || 5500;
 //middlewares
 app.use(cors());
 app.use(express.json());
-// app.use(bodyParser.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
