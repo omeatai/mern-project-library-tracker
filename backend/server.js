691a00478c3863fd2e61495822ca483ebfeae83c
omeatai <omeatai@gmail.com>
2023-11-27 22:51:49 -0700

    changes for Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,9 @@
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-
-require("dotenv").config();
-
 const app = express();
-const port = process.env.PORT || 5500;
 
-const booksRouter = require("./routes/books");
-const usersRouter = require("./routes/users");
-
-//middlewares
-app.use(cors());
-app.use(express.json());
-
-app.use("/books", booksRouter);
-app.use("/users", usersRouter);
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {});
-
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+app.get("/", function (req, res) {
+  res.set("Content-Type", "text/html; charset=utf-8");
+  res.send("<h1>Hello from Node App</h1>");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+app.listen(process.env.PORT || 3000);
