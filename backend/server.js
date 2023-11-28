63a88c55bf5a46353867f0920407e4c47885738a
omeatai <omeatai@gmail.com>
2023-11-27 22:56:53 -0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get("/", function (req, res) {
   res.set("Content-Type", "text/html; charset=utf-8");
-  res.send("<h1>Hello from Node App</h1>");
+  res.send(`<h1>Hello from Node App. Version: ${process.versions.node}</h1>`);
 });
 
 app.listen(process.env.PORT || 3000);
